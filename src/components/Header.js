3321b7350da7a47e57be260355d4bf956c8a8e75
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-08 21:47:26 +0100

    -design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
+import './Header.css';
+
 const Header = () => {
 
     return (
         <>
             <header className="row">
                 <div className="col-sm-8 text-center text-sm-left">
-                    <h1 className="m-0">Task Tracker</h1>
+                    <h1 className="m-0 Header-h1">Task Tracker</h1>
                 </div>
                 <div className="col-sm-4 align-self-center">
-                    <button className="btn btn-primary btn-block">Add</button>
+                    <button className="btn btn-dark btn-block">Add</button>
                 </div>
             </header>
-            <div className="row">
+            <div className="row" aria-hidden="true">
                 <div className="col">
                     <hr />
                 </div>
