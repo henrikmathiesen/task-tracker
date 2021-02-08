831ee28cfedb48731c0b6e1ad087dd58d3a395ae
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-08 21:15:14 +0100

    -added font -button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 const Header = () => {
 
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className="row">
+            <div className="col-sm-6 text-center text-sm-left">
+                <h1>Task Tracker</h1>
+            </div>
+            <div className="col-sm-4">
+                <button className="btn btn-primary btn-block">Add</button>
+            </div>
         </header>
     );
 
