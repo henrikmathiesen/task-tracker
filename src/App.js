2f0f5cd7433349facfc8db0ae1ebbb65b6bf8356
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-09 17:42:50 +0100

    -smaller width on app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Header from './components/Header';
+import './App.css';
 
 const App = () => {
   return (
-    <div className="container py-sm-3">
+    <div className="container py-sm-3 App-container">
       <Header />
     </div>
   );
