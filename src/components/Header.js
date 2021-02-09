6425029977401a965d76fb33040b7b5c0eee64b3
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-09 20:15:03 +0100

    -installed react-icons -design

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ onAddClick }) => {
                     <h1 className="m-0 Header-h1">Task Tracker</h1>
                 </div>
                 <div className="col-sm-4 align-self-center">
-                    <button className="btn btn-dark btn-block" onClick={onAddClick}>Add</button>
+                    <button className="btn btn-dark btn-block Header-add-button" onClick={onAddClick}>Add</button>
                 </div>
             </header>
             <div className="row mb-4" aria-hidden="true">
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,24 @@
 import PropTypes from 'prop-types';
+import { FaTimes } from 'react-icons/fa';
+import './Task.css';
 
 const Task = ({ task, isLast }) => {
 
     const { reminder, day, text } = task;
 
     return (
-        <div className={`card ${isLast === false ? 'mb-3' : ''} ${reminder ? 'border-dark': ''}`}>
-            <div className="card-header d-flex justify-content-between">
-                <div>{day.toDateString()}</div>
-                { reminder &&
-                    <div>
-                        <span className="badge badge-dark">Reminder</span>
-                    </div> }
+        <div className={`card ${!isLast && 'mb-3'} ${reminder && 'border-dark'}`}>
+            <div className="card-header d-flex justify-content-between align-items-center">
+                <div className="Task-day">{day.toDateString()}</div>
+                <div>
+                    {reminder && <span className="badge badge-dark mr-2 Task-reminder">Reminder</span>}
+                    <button className="btn btn-sm text-danger" aria-label="Delete">
+                        <span aria-hidden="true"><FaTimes /></span>
+                    </button>
+                </div>
             </div>
             <div className="card-body">
-                <h2 className="h5 card-title">{text}</h2>
+                <h2 className="h5 card-title Task-card-title">{text}</h2>
             </div>
         </div>
     )
