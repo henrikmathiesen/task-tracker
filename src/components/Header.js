94c723867a1e9e3877b2dd72b99d656afad0b714
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-10 19:23:34 +0100

    -design

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ onAddClick }) => {
         <>
             <header className="row">
                 <div className="col-sm-8 text-center text-sm-left">
-                    <h1 className="m-0 Header-h1">Task Tracker</h1>
+                    <h1 className="m-0 text-danger Header-h1">Task Tracker</h1>
                 </div>
                 <div className="col-sm-4 align-self-center">
                     <button className="btn btn-dark btn-block Header-add-button" onClick={onAddClick}>Add</button>
