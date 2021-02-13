b58ef029ccfeef73318715f56a079f184f5d8336
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-13 10:51:02 +0100

    -design

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,24 +1,37 @@
+import './AddTask.css';
+
 
 const AddTask = () => {
 
     return (
-        <form className="mb-5">
-            <div className="form-group">
-                <label htmlFor="text">Text (required)</label>
-                <textarea id="text" className="form-control" required></textarea>
-            </div>
-            <div className="form-group row">
+        <form className="mb-5" noValidate>
+
+            <div className="row">
                 <div className="col-sm-6">
-                    <label htmlFor="day">Day (required, YYYY-MM-DD)</label>
-                    <input type="text" id="day" className="form-control" required />
+                    <label htmlFor="text">Text (required)</label>
+                    <textarea id="text" className="form-control Add-Task-text-area" required></textarea>
                 </div>
-                <div className="col-sm-6 align-self-end mt-3 mt-sm-0 text-center">
-                    <div className="custom-control custom-switch">
-                        <input type="checkbox" id="reminder" className="custom-control-input" />
-                        <label htmlFor="reminder" className="custom-control-label">Reminder</label>
+                <div className="col-sm-6 mt-3 mt-sm-0">
+                    <div className="row">
+                        <div className="col">
+                            <label htmlFor="day">Day (required, YYYY-MM-DD)</label>
+                            <input type="text" id="day" className="form-control" required />
+                        </div>
+                    </div>
+                    <div className="row mt-3">
+                        <div className="col align-self-center text-center">
+                            <div className="custom-control custom-switch">
+                                <input type="checkbox" id="reminder" className="custom-control-input" />
+                                <label htmlFor="reminder" className="custom-control-label">Reminder</label>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <button className="btn btn-danger btn-block">Go!</button>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </form>
     )
 
