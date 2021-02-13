3aa2bac4680bbdc21cf915b29aaa7463a1048a2e
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-13 10:20:00 +0100

    -design

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,19 +2,21 @@
 const AddTask = () => {
 
     return (
-        <form>
+        <form className="mb-5">
             <div className="form-group">
                 <label htmlFor="text">Text (required)</label>
                 <textarea id="text" className="form-control" required></textarea>
             </div>
-            <div className="form-group">
-                <label htmlFor="day">Day (required)</label>
-                <input type="text" id="day" className="form-control" required />
-            </div>
-            <div className="form-group">
-                <div className="custom-control custom-switch">
-                    <input type="checkbox" id="reminder" className="custom-control-input" />
-                    <label htmlFor="reminder" className="custom-control-label">Reminder</label>
+            <div className="form-group row">
+                <div className="col-sm-6">
+                    <label htmlFor="day">Day (required)</label>
+                    <input type="text" id="day" className="form-control" required />
+                </div>
+                <div className="align-self-end col-sm-6 mt-3 mt-sm-0 text-center">
+                    <div className="custom-control custom-switch">
+                        <input type="checkbox" id="reminder" className="custom-control-input" />
+                        <label htmlFor="reminder" className="custom-control-label">Reminder</label>
+                    </div>
                 </div>
             </div>
         </form>
