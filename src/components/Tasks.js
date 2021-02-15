ae5b0eada0fa0b14b80a6c218df26465cf5b3437
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-15 19:04:58 +0100

    -todo

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,10 @@ import Task from './Task';
 
 const Tasks = ({ tasks, onDeleteClick, onReminderDoubleClick }) => {
 
+    // TODO: do this with proper state instead (check so logging wont execute as much)
     const isLast = (t) => {
+        console.log('isLast');
+
         const lastIndex = tasks.length - 1;
         return tasks.indexOf(t) === lastIndex;
     };
