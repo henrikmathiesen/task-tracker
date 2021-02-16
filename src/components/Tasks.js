f92ba50a837ab44d55397872d27eedb0f7531107
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-02-16 18:47:51 +0100

    -check isLast

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,17 +3,13 @@ import Task from './Task';
 
 const Tasks = ({ tasks, onDeleteClick, onReminderDoubleClick }) => {
 
-    // TODO: do this with proper state instead (check so logging wont execute as much)
-    const isLast = (t) => {
-        console.log('isLast');
-
-        const lastIndex = tasks.length - 1;
-        return tasks.indexOf(t) === lastIndex;
-    };
-
     return (
         <>
-            { tasks.map(t => <Task key={t.id} task={t} isLast={isLast(t)} onDeleteClick={onDeleteClick} onReminderDoubleClick={onReminderDoubleClick} />)}
+            {
+                tasks.map((t, i, arr) => { 
+                    return <Task key={t.id} task={t} isLast={arr.length - 1 === i} onDeleteClick={onDeleteClick} onReminderDoubleClick={onReminderDoubleClick} />
+                })
+            }
         </>
     )
 
